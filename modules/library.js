ebfa521cd1d1a19ee76b084a99cfcfcb1ca42afc
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-06-15 11:17:16 -0600

    Update styles and Remove page reload

--- modules/library.js
+++ modules/library.js
@@ -1,9 +1,6 @@
 class Library {
   constructor() {
-    this.books = [];
-    if (localStorage.getItem('books')) {
-      this.books = JSON.parse(localStorage.getItem('books'));
-    }
+    this.books = JSON.parse(localStorage.getItem('books')) ? JSON.parse(localStorage.getItem('books')) : [];
   }
 
   updateLocalStorage() {
@@ -12,47 +9,34 @@ class Library {
 
   displayBooks() {
     const library = document.getElementById('library');
+    while (library.firstChild) library.removeChild(library.firstChild);
     let count = 1;
     this.books.forEach((book) => {
-      if (count % 2) {
-        book.Number = JSON.stringify(count);
-        library.insertAdjacentHTML('beforeend', `
-          <div class="book gray">
-            <span>"${book.title}" by ${book.author}</span>
-            <button class="remove-btn" data-id="${book.Number}">Remove</button>
-          </div>
-        `);
-        count += 1;
-      } else {
-        book.Number = JSON.stringify(count);
-        library.insertAdjacentHTML('beforeend', `
-          <div class="book">
-            <span>"${book.title}" by ${book.author}</span>
-            <button class="remove-btn" data-id="${book.Number}">Remove</button>
-          </div>
-        `);
-        count += 1;
-      }
+      book.Number = JSON.stringify(count);
+      library.insertAdjacentHTML('beforeend', `
+        <div class="book">
+          <span>"${book.title}" by ${book.author}</span>
+          <button class="remove-btn" data-id="${book.Number}">Remove</button>
+        </div>
+      `);
+      count += 1;
     });
 
     const removeBook = (id) => {
       this.books = this.books.filter((book) => book.Number !== id);
       this.updateLocalStorage();
-      window.location.reload(true);
     };
 
     const removeButtons = document.querySelectorAll('.remove-btn');
     removeButtons.forEach((button) => button.addEventListener('click', (e) => {
       removeBook(e.target.dataset.id);
+      e.target.parentElement.remove();
     }));
   }
 
   addBook(title, author) {
     this.books.push({ title, author });
     this.updateLocalStorage();
-    window.location.reload(true);
   }
 }
-
-const library = new Library();
-export default library;
+export default new Library();
